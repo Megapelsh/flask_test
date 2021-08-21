30cca647898a485d70d162ab5ac7025dff3e3c08
Vlad <tkachenkovladv@gmail.com>
2021-08-22 01:20:15 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+menu = ['Section 1', 'Section 2', 'Section 3']
+
+
+@app.context_processor
+def main_menu():
+    return dict(
+        menu=[{'name': 'Index', 'url': '/'},
+              {'name': 'Hey', 'url': '/hey'},
+              {'name': 'section 3', 'url': '#'}]
+    )
+
+
+@app.route('/')
+def index():  # put application's code here
+    print(url_for('index'))
+    return render_template('index.html')
+
+
+@app.route('/hey')
+def hey():
+    print(url_for('hey'))
+    return render_template('hey.html', title='hey')
+
+
+if __name__ == '__main__':
+    app.run()
